5e3e01b5416375210b6a9c716e52fdc8bfbc4478
Huy Trinh <giahuy.trinh@tuni.fi>
2021-01-16 23:57:53 +0200

    Fix overflow error and add register suffix

--- E06/wave_gen.vhd
+++ E06/wave_gen.vhd
@@ -3,10 +3,10 @@
 -- Project    : 
 -------------------------------------------------------------------------------
 -- File       : wave_gen.vhd
--- Author     :   <ACER@LAPTOP-PGKK1HS3>
+-- Author     :   Trinh Gia Huy
 -- Company    : 
 -- Created    : 2020-12-05
--- Last update: 2020-12-05
+-- Last update: 2021-01-16
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -16,7 +16,7 @@
 -------------------------------------------------------------------------------
 -- Revisions  :
 -- Date        Version        Author                          Description
--- 2020-12-05  1.0              Trinh Huy	                Created
+-- 2020-12-05  1.0            Trinh Huy	                      Created
 -------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
@@ -37,8 +37,8 @@ end entity wave_gen;
 
 architecture generator of wave_gen is
 
-  signal dir : std_logic;
-  signal current_value : signed(width_g -1 downto 0);
+  signal dir_r : std_logic;
+  signal current_value_r : signed(width_g -1 downto 0);
   constant upwards_c : std_logic := '1';
   constant max_c : integer := ((2**(width_g-1)-1)/step_g)*step_g;
 
@@ -47,34 +47,34 @@ begin
   sync : process ( clk , rst_n)
   begin
     if ( rst_n = '0') then
-      current_value <= (others => '0');
-      dir <= upwards_c;
+      current_value_r <= (others => '0');
+      dir_r <= upwards_c;
     elsif ( clk = '1' and clk'event) then
       if ( sync_clear_n_in = '0') then
         --rest the current value to 0 and change the upward direction
-        current_value <= (others => '0');
-        dir <= upwards_c;
+        current_value_r <= (others => '0');
+        dir_r <= upwards_c;
       else
         -- If the current direction is upward and next step lead to max value
         -- or current direction is downward and next step lead to min value
         -- then change the direction
-        if (to_integer(current_value) = max_c - step_g and dir = upwards_c ) then
-          dir <= not upwards_c;
-        elsif ( to_integer(current_value) = max_c + step_g and dir = not upwards_c) then
-          dir <= upwards_c;
+        if (to_integer(current_value_r) = max_c - step_g and dir_r = upwards_c ) then
+          dir_r <= not upwards_c;
+        elsif ( to_integer(current_value_r) = - max_c + step_g and dir_r = not upwards_c) then
+          dir_r <= upwards_c;
         end if;
 
-        if ( dir = upwards_c) then
-          current_value <= current_value + step_g;
+        if ( dir_r = upwards_c) then
+          current_value_r <= current_value_r + step_g;
         else 
-          current_value <= current_value - step_g;
+          current_value_r <= current_value_r - step_g;
         end if;
       
     end if;
   end if;
 end process sync;
 
-value_out <= std_logic_vector(current_value);
+value_out <= std_logic_vector(current_value_r);
 
 end architecture generator;
 
