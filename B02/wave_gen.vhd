c8f7bdcfdb1b58f7b9d05b51c8823c6a27685c7e
Huy Trinh <giahuy.trinh@tuni.fi>
2021-03-07 23:23:03 +0200

    Submit final version of sine wave generator

--- B02/wave_gen.vhd
+++ B02/wave_gen.vhd
@@ -6,7 +6,7 @@
 -- Author     : Trinh Gia Huy, Rajesh Singapati
 -- Company    : 
 -- Created    : 2020-12-05
--- Last update: 2021-03-02
+-- Last update: 2021-03-07
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -21,6 +21,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use IEEE.math_real.all;
 
 
 entity wave_gen is
@@ -41,40 +42,46 @@ architecture generator of wave_gen is
   signal current_value_r : signed(width_g -1 downto 0);
   constant upwards_c : std_logic := '1';
   constant max_c : integer := ((2**(width_g-1)-1)/step_g)*step_g;
+  constant ref_clk_c : integer := 50000000;
+  constant frequency_c : integer := (step_g * 100000);
 
+  constant N_c : integer := ref_clk_c / frequency_c;
+
+  --The incremental value that added to every point
+  signal d_c : integer := (2*2*max_c)/N_c;
+  signal cn_r : integer;
+  signal sn_r : integer;
+                                    
 begin
 
   sync : process ( clk , rst_n)
   begin
     if ( rst_n = '0') then
-      current_value_r <= (others => '0');
-      dir_r <= upwards_c;
+     -- current_value_r <= (others => '0');
+     -- dir_r <= upwards_c;
+      sn_r <= 0;
+      cn_r <= max_c;
     elsif ( clk = '1' and clk'event) then
       if ( sync_clear_n_in = '0') then
-        --rest the current value to 0 and change the upward direction
-        current_value_r <= (others => '0');
-        dir_r <= upwards_c;
+        sn_r <= 0;
       else
-        -- If the current direction is upward and next step lead to max value
-        -- or current direction is downward and next step lead to min value
-        -- then change the direction
-        if (to_integer(current_value_r) = max_c - step_g and dir_r = upwards_c ) then
-          dir_r <= not upwards_c;
-        elsif ( to_integer(current_value_r) = - max_c + step_g and dir_r = not upwards_c) then
-          dir_r <= upwards_c;
-        end if;
-
-        if ( dir_r = upwards_c) then
-          current_value_r <= current_value_r + step_g;
-        else 
-          current_value_r <= current_value_r - step_g;
-        end if;
-      
+        --Generate the next Cn and Sn recursively
+        cn_r <= cn_r - d_c * sn_r / max_c;
+        sn_r <= sn_r + d_c * cn_r / max_c;
+        --Detect the Sn's codition of overflow
+        if sn_r > max_c - d_c + 1 then
+          sn_r <= max_c - d_c + 1;
+          cn_r <= 0;
+        elsif sn_r < -(max_c - d_c - 1) then
+           sn_r <= -(max_c - d_c -1);
+           cn_r <= 0;              
+        end if;     
     end if;
   end if;
 end process sync;
 
-value_out <= std_logic_vector(current_value_r);
+--Assing the Sn register to output of wave generator
+value_out <= std_logic_vector(to_signed(sn_r,width_g));
 
 end architecture generator;
 
